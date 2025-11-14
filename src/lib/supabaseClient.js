884c92f1270be738d73a70e4b291d2a7ea358c34
mustofa08 <tofa354kendal@gmail.com>
2025-11-14 15:53:29 +0700

    update supabase client

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,6 +1,16 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY,
+  {
+    auth: {
+      persistSession: true,
+      autoRefreshToken: true,
+      detectSessionInUrl: true,
+      flowType: "pkce", // WAJIB untuk hosted domain
+    },
+  }
+);
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export { supabase };
